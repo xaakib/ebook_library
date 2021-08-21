739ff4f2feab2201b40b5c2729d28a30973791dc
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-08-21 15:53:33 +0600

    discoverded screen not done

---
+++ lib/style/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+TextStyle texstyleBoldBlack =
+    TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18);
+    
+TextStyle texstyleBoldBlack16 =
+    TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 16);
+
+TextStyle textsyleGreyNormal =
+    TextStyle(fontWeight: FontWeight.normal, color: Colors.grey);
