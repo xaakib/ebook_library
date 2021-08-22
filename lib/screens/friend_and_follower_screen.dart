5ac22b6ae7bc25605f01151323abe892f5dc4f64
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-08-22 14:47:34 +0600

    friends tab screen ui done

--- lib/screens/friend_and_follower_screen.dart
+++ lib/screens/friend_and_follower_screen.dart
@@ -10,10 +10,6 @@ class FriendAndFollowerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double mediaQueryHeightScreen = MediaQuery.of(context).size.height;
-
-    double mediaQueryWidthScreen = MediaQuery.of(context).size.width;
-
     return DefaultTabController(
       length: 3,
       child: Scaffold(
